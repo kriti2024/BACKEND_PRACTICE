b01642d58dafacff3665c6b5493c2c63070ef01c
Kriti Tripathi <tripathikriti483@gmail.com>
2025-08-20 15:49:45 +0545

    Convert index.js to Typescript and update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,9 @@
 import { Router } from "express"; 
-import userRoutes from "./user.route.js"
+import userRoutes from "./user.route";
 
-const router = Router()
+const router: Router = Router()
+
 
 router.use("/user", userRoutes)
 
-export default router
+export default router;
